91de9f392cb3468d3ebb13ac875a402adfee5ac0
Savelii Sidorkin <ssd-19@yandex.ru>
2024-02-06 01:31:42 +0300

    4ый коммит

--- src/main/java/model/Food.java
+++ src/main/java/model/Food.java
@@ -8,7 +8,7 @@ public abstract class Food implements Discountable {
     public Food(int amount, double price, boolean isVegetarian) {
         this.amount = amount;
         this.price = price;
-        this.isVegetarian = isVegetarian;//11юдждло
+        this.isVegetarian = isVegetarian;//11юдждлооррр
     }
 
     @Override
